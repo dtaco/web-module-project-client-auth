bb24e7f7155ad27b3ab1434776f99091b5922649
David Tacoronte <dtaco@MacBook-Pro-de-David.local>
2023-10-24 14:11:38 -0400

    installed dependencies, mocked up skeleton, created routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
+
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+
+const FriendsList = () => {
+  return (<h2>Friends List</h2>)
+}
+
+const AddFriend = () => {
+  return (<h2>Add Friend!</h2>)
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+      <div className="App">
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/friends" element={<FriendsList/>}/>
+        <Route path="/friendsadd" element={<AddFriend/>}/>
+      </Routes>
+      </div>
   );
 }
 
